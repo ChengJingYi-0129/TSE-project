0aa15429b198a8420eb2b8af6373b027d2d21d6f
Fernando040409 <129022113+Fernando040409@users.noreply.github.com>
2025-05-13 23:30:10 +0800

    update

--- send_otp.php
+++ send_otp.php
@@ -6,8 +6,11 @@ if (isset($_POST['email']) && isset($_POST['otp']) && isset($_POST['expiry'])) {
     $expiry = $_POST['expiry'];
     $message = "Your OTP is: $otp. Valid 10 minutes until $expiry";
     $headers = "From: noreply@mmu.edu.my\r\n";
-    mail($to, $subject, $message, $headers);
-    echo "sent";
+    if (mail($to, $subject, $message, $headers)) {
+        echo "sent";
+    } else {
+        echo "mail_failed";
+    }
 } else {
     echo "error";
 }
